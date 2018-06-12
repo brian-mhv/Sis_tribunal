48cd64cb0012c5a2c77a65adf1b4c60620d9200d
Alejandra Montano <Alejandra.Montano@fundacion-jala.org>
2018-06-12 11:15:48 -0400

    tablas que empiecen con minusculas

--- proyectoTIS/Sql/tis_db.sql
+++ proyectoTIS/Sql/tis_db.sql
@@ -5,33 +5,33 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 CREATE DATABASE  IF NOT EXISTS hashtag_db CHARACTER SET utf8 COLLATE utf8_spanish2_ci;
 USE hashtag_db;
 
-DROP TABLE IF EXISTS `TipoResponsable` ;
-DROP TABLE IF EXISTS `EstCarrera` ;
-DROP TABLE IF EXISTS `ProfTesis` ;
-DROP TABLE IF EXISTS `EstTesis` ;
-DROP TABLE IF EXISTS `Titulo` ;
-DROP TABLE IF EXISTS `Docente` ;
-DROP TABLE IF EXISTS `Profesional` ;
-DROP TABLE IF EXISTS `Modalidad` ;
-DROP TABLE IF EXISTS `GestionPer` ;
-DROP TABLE IF EXISTS `Tesis` ;
-DROP TABLE IF EXISTS `Tribunal` ;
-DROP TABLE IF EXISTS `Estudiante` ;
-DROP TABLE IF EXISTS `Sesion` ;
-DROP TABLE IF EXISTS `Area` ;
-DROP TABLE IF EXISTS `AreasProfesional` ;
-DROP TABLE IF EXISTS `AreaTesis` ;
-DROP TABLE IF EXISTS `Carrera` ;
+DROP TABLE IF EXISTS `tiporesponsable` ;
+DROP TABLE IF EXISTS `estcarrera` ;
+DROP TABLE IF EXISTS `proftesis` ;
+DROP TABLE IF EXISTS `esttesis` ;
+DROP TABLE IF EXISTS `titulo` ;
+DROP TABLE IF EXISTS `docente` ;
+DROP TABLE IF EXISTS `profesional` ;
+DROP TABLE IF EXISTS `modalidad` ;
+DROP TABLE IF EXISTS `gestionper` ;
+DROP TABLE IF EXISTS `tesis` ;
+DROP TABLE IF EXISTS `tribunal` ;
+DROP TABLE IF EXISTS `estudiante` ;
+DROP TABLE IF EXISTS `sesion` ;
+DROP TABLE IF EXISTS `area` ;
+DROP TABLE IF EXISTS `areasprofesional` ;
+DROP TABLE IF EXISTS `areatesis` ;
+DROP TABLE IF EXISTS `carrera` ;
 
-CREATE  TABLE IF NOT EXISTS `TipoResponsable` (
+CREATE  TABLE IF NOT EXISTS `tiporesponsable` (
   `id_tipo` INT NOT NULL ,
   `nombre_tipo` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id_tipo`) )
 ENGINE = InnoDB;
 -- -----------------------------------------------------
--- Table `EstCarrera`
+-- Table `estcarrera`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `EstCarrera` (
+CREATE  TABLE IF NOT EXISTS `estcarrera` (
   `cod_est` INT NOT NULL ,
   `cod_carrera` INT NOT NULL ,
   PRIMARY KEY (`cod_est`, `cod_carrera`) ,
@@ -39,20 +39,20 @@ CREATE  TABLE IF NOT EXISTS `EstCarrera` (
   INDEX `cod_est_idx` (`cod_est` ASC) ,
   CONSTRAINT `cod_est`
     FOREIGN KEY (`cod_est` )
-    REFERENCES `Estudiante` (`codigo` )
+    REFERENCES `estudiante` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `cod_carrera`
     FOREIGN KEY (`cod_carrera` )
-    REFERENCES `Carrera` (`codigo` )
+    REFERENCES `carrera` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `ProfTesis`
+-- Table `proftesis`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `ProfTesis` (
+CREATE  TABLE IF NOT EXISTS `proftesis` (
   `cod_prof` INT NOT NULL ,
   `cod_tesis` INT NOT NULL ,
   `tipo_resp` INT NOT NULL,
@@ -62,25 +62,25 @@ CREATE  TABLE IF NOT EXISTS `ProfTesis` (
   INDEX `tipo_resp_idx` (`tipo_resp` ASC) ,
   CONSTRAINT `cod_prof`
     FOREIGN KEY (`cod_prof`)
-    REFERENCES `Profesional` (`codigo` )
+    REFERENCES `profesional` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `cod_tesis`
     FOREIGN KEY (`cod_tesis` )
-    REFERENCES `Tesis` (`codigo` )
+    REFERENCES `tesis` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `tipo_resp`
     FOREIGN KEY (`tipo_resp` )
-    REFERENCES `TipoResponsable` (`id_tipo` )
+    REFERENCES `tiporesponsable` (`id_tipo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `EstTesis`
+-- Table `esttesis`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `EstTesis` (
+CREATE  TABLE IF NOT EXISTS `esttesis` (
   `cod_alumno` INT NOT NULL ,
   `cod_tes` INT NOT NULL ,
   PRIMARY KEY (`cod_alumno`, `cod_tes`) ,
@@ -88,29 +88,29 @@ CREATE  TABLE IF NOT EXISTS `EstTesis` (
   INDEX `cod_alumno_idx` (`cod_alumno` ASC) ,
   CONSTRAINT `cod_alumno`
     FOREIGN KEY (`cod_alumno` )
-    REFERENCES `Estudiante` (`codigo` )
+    REFERENCES `estudiante` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `cod_tes`
     FOREIGN KEY (`cod_tes` )
-    REFERENCES `Tesis` (`codigo` )
+    REFERENCES `tesis` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Titulo`
+-- Table `titulo`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Titulo` (
+CREATE  TABLE IF NOT EXISTS `titulo` (
   `codigo` INT NOT NULL ,
   `nombre` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`codigo`) )
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Docente`
+-- Table `docente`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Docente` (
+CREATE  TABLE IF NOT EXISTS `docente` (
   `codigo` INT NOT NULL AUTO_INCREMENT ,
   `carga_horaria` VARCHAR(45) NULL ,
   `telefono` VARCHAR(8) NULL ,
@@ -124,9 +124,9 @@ CREATE  TABLE IF NOT EXISTS `Docente` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Profesional`
+-- Table `profesional`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Profesional` (
+CREATE  TABLE IF NOT EXISTS `profesional` (
   `codigo` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(45) NOT NULL ,
   `apellido_paterno` VARCHAR(45) NULL ,
@@ -140,20 +140,20 @@ CREATE  TABLE IF NOT EXISTS `Profesional` (
   UNIQUE INDEX `cod_docente_UNIQUE` (`cod_docente` ASC) ,
   CONSTRAINT `titulo`
     FOREIGN KEY (`titulo` )
-    REFERENCES `Titulo` (`codigo` )
+    REFERENCES `titulo` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `cod_docente`
     FOREIGN KEY (`cod_docente` )
-    REFERENCES `Docente` (`codigo` )
+    REFERENCES `docente` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Modalidad`
+-- Table `modalidad`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Modalidad` (
+CREATE  TABLE IF NOT EXISTS `modalidad` (
   `codigo` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(45) NOT NULL ,
   `descripcion` VARCHAR(1000) NULL ,
@@ -162,9 +162,9 @@ CREATE  TABLE IF NOT EXISTS `Modalidad` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `GestionPer`
+-- Table `gestionper`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `GestionPer` (
+CREATE  TABLE IF NOT EXISTS `gestionper` (
   `codigo` INT NOT NULL AUTO_INCREMENT ,
   `fecha_ini` DATE NULL ,
   `fecha_fin` DATE NULL ,
@@ -174,9 +174,9 @@ CREATE  TABLE IF NOT EXISTS `GestionPer` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Tesis`
+-- Table `tesis`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Tesis` (
+CREATE  TABLE IF NOT EXISTS `tesis` (
   `codigo` INT NOT NULL AUTO_INCREMENT ,
   `codigo_tesis` VARCHAR(45) NULL ,
   `nombre` VARCHAR(450) NOT NULL ,
@@ -198,31 +198,31 @@ CREATE  TABLE IF NOT EXISTS `Tesis` (
   INDEX `carrera_idx` (`carrera` ASC),
   CONSTRAINT `cod_modalidad`
     FOREIGN KEY (`cod_modalidad` )
-    REFERENCES `Modalidad` (`codigo` )
+    REFERENCES `modalidad` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `cod_ges_per_ini`
     FOREIGN KEY (`cod_ges_per_ini` )
-    REFERENCES `GestionPer` (`codigo` )
+    REFERENCES `gestionper` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `cod_ges_per_fin`
     FOREIGN KEY (`cod_ges_per_fin` )
-    REFERENCES `GestionPer` (`codigo` )
+    REFERENCES `gestionper` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
 CONSTRAINT `carrera`
     FOREIGN KEY (`carrera` )
-    REFERENCES `Carrera` (`codigo` )
+    REFERENCES `carrera` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Tribunal`
+-- Table `tribunal`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Tribunal` (
-  `idTribunal` INT NOT NULL AUTO_INCREMENT ,
+CREATE  TABLE IF NOT EXISTS `tribunal` (
+  `idtribunal` INT NOT NULL AUTO_INCREMENT ,
   `id_profesional1` INT NOT NULL ,
   `id_profesional2` INT NOT NULL ,
   `id_profesional3` INT NOT NULL ,
@@ -237,30 +237,30 @@ CREATE  TABLE IF NOT EXISTS `Tribunal` (
   INDEX `profesional3_idx` (`id_profesional3` ASC) ,
   CONSTRAINT `profesional1`
     FOREIGN KEY (`id_profesional1` )
-    REFERENCES `Profesional` (`codigo` )
+    REFERENCES `profesional` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `id_proyecto`
     FOREIGN KEY (`id_tesis` )
-    REFERENCES `Tesis` (`codigo` )
+    REFERENCES `tesis` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `profesional2`
     FOREIGN KEY (`id_profesional2` )
-    REFERENCES `Profesional` (`codigo` )
+    REFERENCES `profesional` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `profesional3`
     FOREIGN KEY (`id_profesional3` )
-    REFERENCES `Profesional` (`codigo` )
+    REFERENCES `profesional` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Estudiante`
+-- Table `estudiante`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Estudiante` (
+CREATE  TABLE IF NOT EXISTS `estudiante` (
   `codigo` INT NOT NULL AUTO_INCREMENT ,
   `cod_sis` INT NULL ,
   `nombre` VARCHAR(45) NOT NULL ,
@@ -278,9 +278,9 @@ CREATE  TABLE IF NOT EXISTS `Estudiante` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Sesion`
+-- Table `sesion`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Sesion` (
+CREATE  TABLE IF NOT EXISTS `sesion` (
   `usuario` INT NOT NULL ,
   `correo` VARCHAR(45) NOT NULL,
   `pass` VARCHAR(45) NULL DEFAULT 'hashtag',
@@ -290,10 +290,10 @@ CREATE  TABLE IF NOT EXISTS `Sesion` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Area`
+-- Table `area`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Area` (
-  `idArea` INT NOT NULL AUTO_INCREMENT ,
+CREATE  TABLE IF NOT EXISTS `area` (
+  `idarea` INT NOT NULL AUTO_INCREMENT ,
   `nombre_area` VARCHAR(1000)  NULL,
   `descripcion` VARCHAR(5000) NULL DEFAULT 'Descripcion no disponible' ,
   `id_subarea` INT NULL,
@@ -303,9 +303,9 @@ CREATE  TABLE IF NOT EXISTS `Area` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `AreasProfesional`
+-- Table `areasprofesional`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `AreasProfesional` (
+CREATE  TABLE IF NOT EXISTS `areasprofesional` (
   `id_profesional` INT NOT NULL ,
   `id_area` INT NOT NULL ,
   INDEX `area_idx` (`id_area` ASC) ,
@@ -313,20 +313,20 @@ CREATE  TABLE IF NOT EXISTS `AreasProfesional` (
   INDEX `profesional_idx` (`id_profesional` ASC) ,
   CONSTRAINT `area`
     FOREIGN KEY (`id_area` )
-    REFERENCES `Area` (`idArea` )
+    REFERENCES `area` (`idArea` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `profesional`
     FOREIGN KEY (`id_profesional` )
-    REFERENCES `Profesional` (`codigo` )
+    REFERENCES `profesional` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `AreaTesis`
+-- Table `areatesis`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `AreaTesis` (
+CREATE  TABLE IF NOT EXISTS `areatesis` (
   `id_area` INT NOT NULL ,
   `id_tesis` INT NOT NULL ,
   INDEX `area_idx` (`id_area` ASC) ,
@@ -334,20 +334,20 @@ CREATE  TABLE IF NOT EXISTS `AreaTesis` (
   PRIMARY KEY (`id_tesis`, `id_area`) ,
   CONSTRAINT `area0`
     FOREIGN KEY (`id_area` )
-    REFERENCES `Area` (`idArea` )
+    REFERENCES `area` (`idArea` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `proyecto`
     FOREIGN KEY (`id_tesis` )
-    REFERENCES `Tesis` (`codigo` )
+    REFERENCES `tesis` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `Carrera`
+-- Table `carrera`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `Carrera` (
+CREATE  TABLE IF NOT EXISTS `carrera` (
   `codigo` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(50) NOT NULL ,
   `descripcion` VARCHAR(200) NULL ,

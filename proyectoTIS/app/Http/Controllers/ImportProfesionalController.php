9ecdd589f96a2f461e6319e0fe1635cd59645656
Alejandra Montano <Alejandra.Montano@fundacion-jala.org>
2018-06-15 11:01:33 -0400

    changes

--- proyectoTIS/app/Http/Controllers/ImportProfesionalController.php
+++ proyectoTIS/app/Http/Controllers/ImportProfesionalController.php
@@ -8,43 +8,23 @@ use App\Profesional;
 use App\Http\Controllers\Controller;
 use Maatwebsite\Excel\Facades\Excel;
 
-class ImportProfesionalController extends Controller
-{
-    public function import()
-    {
-    	Excel::load('profesionales.csv', function($reader) {
- 
-     foreach ($reader->get() as $profesional) {
-     Profesional::create([
-        'codigo' => $profesional->codigo,
-        'nombre' => $profesional->nombre,
-        'apellido_paterno' => $profesional->ape_pat,
-        'apellido_materno' => $profesional->ape_mat,
-        'titulo' => $profesional->cod_tit,
-        'correo' => $profesional->correo,
-        'cod_docente' => $profesional->cod_docente
-     ]);
-       }
- });
- return Profesional::all();
-    }
+class ImportProfesionalController extends Controller {
 
-    public function importProfesionales($file)
-    {
+    public function importProfesionales($file) {
     	Excel::load($file, function($reader) {
  
-     foreach ($reader->get() as $profesional) {
-     Profesional::create([
-        'codigo' => $profesional->codigo,
-        'nombre' => $profesional->nombre,
-        'apellido_paterno' => $profesional->apellido_paterno,
-        'apellido_materno' => $profesional->apellido_materno,
-        'titulo' => $profesional->titulo,
-        'correo' => $profesional->correo,
-        'cod_docente' => $profesional->cod_docente
-     ]);
-       }
- });
- return Profesional::all();
+        foreach ($reader->get() as $profesional) {
+          Profesional::create([
+            'codigo' => $profesional->codigo,
+            'nombre' => $profesional->nombre,
+            'apellido_paterno' => $profesional->apellido_paterno,
+            'apellido_materno' => $profesional->apellido_materno,
+            'titulo' => $profesional->titulo,
+            'cod_docente' => $profesional->cod_docente,
+            'correo' => $profesional->correo
+          ]);
+        }
+      });
+    return Profesional::all();
     }
 }
